9e3dd8d5642d24d7ff11e721420f0ea8e559ed4d
JPPhoto <jpollack@jpollackphoto.com>
2025-04-27 22:07:34 -0500

    Simplified and corrected some math to better preserve the aspect ratio.

--- flux_ideal_size.py
+++ flux_ideal_size.py
@@ -21,7 +21,7 @@ class FluxIdealSizeOutput(BaseInvocationOutput):
     height: int = OutputField(description="The ideal height of the image in pixels")
 
 
-@invocation("flux_ideal_size", title="Flux Ideal Size", tags=["math", "ideal_size", "flux"], version="1.0.0")
+@invocation("flux_ideal_size", title="Flux Ideal Size", tags=["math", "ideal_size", "flux"], version="1.0.1")
 class FluxIdealSizeInvocation(BaseInvocation):
     """Calculates the ideal size for generation to avoid duplication"""
 
@@ -29,8 +29,9 @@ class FluxIdealSizeInvocation(BaseInvocation):
     height: int = InputField(default=576, description="Target height")
     multiplier: float = InputField(default=1.0, description="Dimensional multiplier")
 
-    def trim_to_multiple_of(self, *args, multiple_of=16):
-        return tuple((x - x % multiple_of) for x in args)
+    def trim_to_multiple_of(self, x, multiple_of=16):
+        x = math.floor(x)
+        return int(x - x % multiple_of)
 
     def invoke(self, context: InvocationContext) -> FluxIdealSizeOutput:
         aspect = self.width / self.height
@@ -40,15 +41,10 @@ class FluxIdealSizeInvocation(BaseInvocation):
         model_area = dimension * dimension  # hardcoded for now since all models are trained on square images
 
         if aspect > 1.0:
-            init_height = max(min_dimension, math.sqrt(model_area / aspect))
-            init_width = init_height * aspect
+            scaled_height = self.trim_to_multiple_of(max(min_dimension, math.sqrt(model_area / aspect)))
+            scaled_width = self.trim_to_multiple_of(scaled_height * aspect)
         else:
-            init_width = max(min_dimension, math.sqrt(model_area * aspect))
-            init_height = init_width / aspect
-
-        scaled_width, scaled_height = self.trim_to_multiple_of(
-            math.floor(init_width),
-            math.floor(init_height),
-        )
+            scaled_width = self.trim_to_multiple_of(max(min_dimension, math.sqrt(model_area * aspect)))
+            scaled_height = self.trim_to_multiple_of(scaled_width / aspect)
 
         return FluxIdealSizeOutput(width=scaled_width, height=scaled_height)
